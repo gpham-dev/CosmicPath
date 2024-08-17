5ad263c4f31b8b7d81eec1182a1df4ab959f36d2
Gierado Pham <gieradopham25@Gierados-MBP.home>
2024-08-16 23:55:54 -0700

    Adding planetary data module

---
+++ planet_data.py
@@ -0,0 +1,61 @@
+"""
+planet_data.py
+
+Contains data and constants for the planetary simulation,
+including real values for planetary radii, distances, 
+and other physical properties.
+
+Author: Gierado Pham
+Date: 2024-08-16
+"""
+
+# Astronimical Units (AU) is the measure distance from planet Earth to the Sun
+
+AU = 149e6 # in kilometers
+
+# Sun Data
+SUN = {
+    'radius':696340, # in kilometers
+    'mass': 1.98892e30 # in kilometers
+}
+
+# Planet Data
+MERCURY = {
+    'radius':2439.7, # in kilometers
+    'mass': 3.30e23 # in kilometers
+}
+
+VENUS = {
+    'radius':, # in kilometers
+    'mass':  # in kilometers
+}
+
+EARTH = {
+    'radius':, # in kilometers
+    'mass':  # in kilometers
+}
+
+MARS = {
+    'radius':, # in kilometers
+    'mass':  # in kilometers
+}
+
+JUPITER = {
+    'radius':, # in kilometers
+    'mass':  # in kilometers
+}
+
+SATURN = {
+    'radius':, # in kilometers
+    'mass':  # in kilometers
+}
+
+URANUS = {
+    'radius':, # in kilometers
+    'mass':  # in kilometers
+}
+
+NEPTUNE = {
+    'radius':, # in kilometers
+    'mass':  # in kilometers
+}
